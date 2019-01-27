a251d26916e9447a4878297a0b6017e452c95d98
German Voytekhovich <ueuknjg@gmail.com>
2019-01-27 15:04:04 +0300

    rm build

--- build/routers/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const router = require('express').Router();
-router.use('/api', require('./api'));
-exports.default = router;
